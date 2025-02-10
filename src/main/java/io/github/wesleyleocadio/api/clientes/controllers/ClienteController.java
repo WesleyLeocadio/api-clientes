40405a817f723d4f87af6ae669e02e50181e7499
Wesley Leocadio <silvawesley374@gmail.com>
2025-02-10 16:48:48 -0300

    Deletando um cliente

--- src/main/java/io/github/wesleyleocadio/api/clientes/controllers/ClienteController.java
+++ src/main/java/io/github/wesleyleocadio/api/clientes/controllers/ClienteController.java
@@ -6,16 +6,19 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import io.github.wesleyleocadio.api.clientes.entity.Cliente;
 import io.github.wesleyleocadio.api.clientes.service.ClienteService;
+import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -37,11 +40,21 @@ public class ClienteController {
     }
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Cliente> buscarPorId(@PathVariable("id") Integer id) {
+	public ResponseEntity<Cliente> buscarPorId(@PathVariable Integer id) {
 	    Optional<Cliente> clienteConsultado = clienteService.buscarPorId(id);
 	    return clienteConsultado
 	            .map(cliente -> ResponseEntity.ok(cliente))  // Retorna o cliente encontrado com status 200 (OK)
 	            .orElseGet(() -> ResponseEntity.notFound().build());  // Retorna 404 (Not Found) caso o cliente não seja encontrado
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+	    try {
+	        clienteService.deletar(id);
+	        return ResponseEntity.noContent().build();  // Status 204 (sem conteúdo) após exclusão
+	    } catch (EntityNotFoundException e) {
+	        throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+	    }
 	}
 	
 }
--- src/main/java/io/github/wesleyleocadio/api/clientes/service/ClienteService.java
+++ src/main/java/io/github/wesleyleocadio/api/clientes/service/ClienteService.java
@@ -3,10 +3,13 @@ package io.github.wesleyleocadio.api.clientes.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import io.github.wesleyleocadio.api.clientes.entity.Cliente;
 import io.github.wesleyleocadio.api.clientes.repository.ClienteRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ClienteService {
@@ -20,6 +23,13 @@ public class ClienteService {
 
 	public Optional<Cliente> buscarPorId(Integer id) {
 		return clienteRepository.findById(id);
+	}
+
+	public void deletar(Integer id) {
+	    buscarPorId(id).map(cliente -> {
+	        clienteRepository.delete(cliente);
+	        return Void.TYPE;
+	    }).orElseThrow(() -> new EntityNotFoundException("Cliente não encontrado com o ID: " + id));
 	}
 	
 }
